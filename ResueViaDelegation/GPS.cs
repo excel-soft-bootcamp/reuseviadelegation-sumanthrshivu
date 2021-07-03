55f7b2a9ce7b86bb8654c42d93e91bf148651357
sumanthrshivu <74582120+sumanthrshivu@users.noreply.github.com>
2021-07-03 14:39:54 +0530

    ReuseViaDelegation

---
+++ ResueViaDelegation/GPS.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResueViaDelegation
+{
+    public class GPS : IGPS
+    {
+        public void Navigate() { }
+    }
+}
---
+++ ResueViaDelegation/IPhone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResueViaDelegation
+{
+    public interface IPhone
+    {
+        void Hangup();
+        void Release();
+    }
+}
